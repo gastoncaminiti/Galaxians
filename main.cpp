6fa8495858037c359f41876fab69e8554eec4e94
Gastón Caminiti <Gastn Caminiti>
2016-10-09 23:18:42 -0300

    Base Enemigos y Polimorfismo

--- main.cpp
+++ main.cpp
@@ -42,16 +42,20 @@ void bala::eliminar(){
 
 //CLASE NAVE BASE
 class nave: public bala{
+	
+protected:
 	int  x,y;
 	int img[4][5];
 	int vida;
 	bala *disparo = NULL;
 public:
 	bala b;
-	nave(int _x, int _y, int _vida):x(_x),y(_y),vida(_vida){};
 	int getX(){return x;};
 	int getY(){return y;};
-	void pintar();
+	void setX(int _x){x=_x;};
+	void setY(int _y){y=_y;};
+	void setVida(int _vida){vida=_vida;};
+	void virtual pintar();
 	void borrar();
 	void mover(char t);
 	void pintar_vida();
@@ -107,15 +111,18 @@ void nave::pintar_vida(){
 
 
 void nave::disparar(){
-	disparo = new bala();
-	disparo->setX(x+2);
-	disparo->setY(y-1);
+	if(!disparo){	
+		disparo = new bala();
+		disparo->setX(x+2);
+		disparo->setY(y-1);
+	}
 }
 
 void nave::mover_disparo(){
 	if(disparo){
 		if(disparo->limite()){
 			disparo->eliminar();
+			disparo=NULL;
 		}else{
 			disparo->mover();
 		}
@@ -139,23 +146,55 @@ void pintar_limites(){
 	gotoxy(77,33); printf("%c",188);
 }
 
+//CLASE NAVE ENEMIGO
+class enemigo_infanteria: virtual public nave{
+	
+public:
+	void pintar();
+	void mover_enemigo();	
+};
+
+void enemigo_infanteria::pintar(){
+	img[0][0]=0;img[0][1]=0;img[0][2]=219;img[0][3]=0;img[0][4]=0;
+	img[1][0]=0;img[1][1]=178;img[1][2]=207;img[1][3]=178;img[1][4]=0;
+	img[2][0]=0;img[2][1]=219;img[2][2]=167;img[2][3]=219;img[2][4]=0;
+	img[3][0]=219;img[3][1]=177;img[3][2]=0;img[3][3]=177;img[3][4]=219;
+	textcolor(4);
+	for(int i=0;i<5;i++){
+		for(int j=0;j<4;j++){
+			gotoxy(x+i,y+j);
+			printf("%c",img[j][i]);
+		}
+		
+	}
+}
+
+void enemigo_infanteria::mover_enemigo(){
+	
+}
+
 int main(int argc, char *argv[]) {
 	pintar_limites();
-	nave n1(38,28,3);
-	n1.pintar();
-	n1.pintar_vida();
+	nave *n1 = new nave();
+	n1->setX(38);n1->setY(28);n1->setVida(3);
+	n1->pintar();
+	n1->pintar_vida();
+	//enemigo
+	nave *n2 = new enemigo_infanteria();
+	n2->setX(12);n2->setY(25);n2->setVida(1);
+	n2->pintar();
 	while(true){
 		_setcursortype( _NOCURSOR );
 		if(kbhit())
 		{ 	
 			char t = getch();
-			n1.borrar();
-			n1.mover(t);
+			n1->borrar();
+			n1->mover(t);
 			if(t == 'a')
-			   n1.disparar();
+			   n1->disparar();
 			
 		}
-		n1.mover_disparo();
+		n1->mover_disparo();
 		Sleep(30);
 		
 	}
