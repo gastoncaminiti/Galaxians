888c591c5276a76e15373cae09731a0178108466
gastoncaminiti <gastoncaminiti@outlook.com>
2017-09-19 20:43:01 -0300

    Fix movimiento en oleada, Disparo player y Colisión.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,13 @@
 #define DER 77
 #define IZQ 75
 
+#define LIMITE_IZQ_OLEADA 6
+#define LIMITE_DER_OLEADA 90
+#define ALTURA_OLEADA 20
+
+#define ALTURA_NAVE 2
+#define ANCHO_NAVE 5
+
 using namespace std;
 
 void ShowConsoleCursor(bool showFlag)
@@ -33,11 +40,12 @@ public:
 };
 
 void bala::mover(){
+	textcolor(15);
 	gotoxy(x,y);
 	printf(" ");
 	if(y>4)y--;
 	gotoxy(x,y);
-	printf("*");
+	printf("%c",248);
 }
 
 bool bala::limite(){
@@ -60,11 +68,14 @@ protected:
 	bala *disparo = NULL;
 	int color;
 	int img[2][5];
+	bool b_activa;
 public:
 	bala b;
 	nave();
 	int getX(){return x;};
 	int getY(){return y;};
+	int getX_B(){return disparo->getX();};
+	int getY_B(){return disparo->getY();};
 	void setX(int _x){x=_x;};
 	void setY(int _y){y=_y;};
 	void setVida(int _vida){vida=_vida;};
@@ -74,18 +85,21 @@ public:
 	void pintar_vida();
 	void disparar();
 	void mover_disparo();
+	void eliminar_disparo();
+	bool bala_activa(){return b_activa;};
+	bool colision_nave(int _x, int _y);
 };
 
 
 nave::nave(){
-	img[0][0]=0;img[0][1]=194;img[0][2]=207;img[0][3]=194;img[0][4]=0;
-	img[1][0]=207;img[1][1]=178;img[1][2]=184;img[1][3]=178;img[1][4]=207;
+	img[0][0]=201;img[0][1]=203;img[0][2]=206;img[0][3]=203;img[0][4]=187;
+	img[1][0]=200;img[1][1]=188;img[1][2]=248;img[1][3]=200;img[1][4]=188;
 	color=6;
 }
 void nave::pintar(){
 	textcolor(color);
-	for(int i=0;i<5;i++){
-		for(int j=0;j<2;j++){
+	for(int i=0;i<ANCHO_NAVE;i++){
+		for(int j=0;j<ALTURA_NAVE;j++){
 			gotoxy(x+i,y+j);
 			printf("%c",img[j][i]);
 		}
@@ -94,8 +108,8 @@ void nave::pintar(){
 }
 
 void nave::borrar(){
-	for(int i=0;i<5;i++){
-		for(int j=0;j<2;j++){
+	for(int i=0;i<ANCHO_NAVE;i++){
+		for(int j=0;j<ALTURA_NAVE;j++){
 			gotoxy(x+i,y+j);
 			printf(" ");
 		}
@@ -126,39 +140,48 @@ void nave::pintar_vida(){
 
 
 void nave::disparar(){
-	if(!disparo){	
+	if(!b_activa){	
 		disparo = new bala();
 		disparo->setX(x+2);
 		disparo->setY(y-1);
+		b_activa = true;
 	}
 }
 
 void nave::mover_disparo(){
-	if(disparo){
-		if(disparo->limite()){
-			disparo->eliminar();
-			disparo=NULL;
-		}else{
-			disparo->mover();
-		}
+	if(b_activa){
+		if(!disparo->limite()) disparo->mover();
+		else eliminar_disparo();
 	}
 }
 
+void nave::eliminar_disparo(){
+	disparo->eliminar();
+	//delete disparo;
+	b_activa = false;
+}
+
 void pintar_limites(){
-	for(int i=2;i<78;i++){
+	for(int i=2;i<100;i++){
 		gotoxy(i,3); printf("%c",205);
 		gotoxy(i,33); printf("%c",205);
 	}
 	
 	for(int i=4;i<33;i++){
 		gotoxy(2,i); printf("%c",186);
-		gotoxy(77,i); printf("%c",186);
+		gotoxy(100,i); printf("%c",186);
 	}
 	
 	gotoxy(2,3); printf("%c",201);
 	gotoxy(2,33); printf("%c",200);
-	gotoxy(77,3); printf("%c",187);
-	gotoxy(77,33); printf("%c",188);
+	gotoxy(100,3); printf("%c",187);
+	gotoxy(100,33); printf("%c",188);
+}
+
+
+bool nave::colision_nave(int _x, int _y){
+	if( _x >= x && _x <= x+ANCHO_NAVE && _y == y )return true;
+	return  false;
 }
 
 //CLASE NAVE ENEMIGA BASE
@@ -176,18 +199,21 @@ public:
 };
 
 void enemigo::mover(){
+	
 	switch(dir)
 	{
 	case IZQ:
-		if(x-(posicion*5)>6)x--;
-		if(x-(posicion*5)==6)dir=DER;
+			if( x - (posicion * (ANCHO_NAVE + 1)) > LIMITE_IZQ_OLEADA) x--;
+			else dir=DER;
 		break;
 	case DER:
-		if(x+(5*(10-posicion))<70)x++;
-		if(x+(5*(10-posicion))==70)dir=IZQ;
+			if(((10-posicion) * (ANCHO_NAVE + 1) ) + x < LIMITE_DER_OLEADA) x++ ;
+			else dir=IZQ;
 		break;
 	}
+	
 	pintar();
+	
 }
 
 //ENEMIGO 1
@@ -197,7 +223,7 @@ public:
 };
 
 enemigo_infanteria::enemigo_infanteria(){
-	img[0][0]=0;img[0][1]=178;img[0][2]=207;img[0][3]=178;img[0][4]=0;
+	img[0][0]=204;img[0][1]=205;img[0][2]=207;img[0][3]=205;img[0][4]=185;
 	img[1][0]=0;img[1][1]=0;img[1][2]=0;img[1][3]=0;img[1][4]=0;
 	color=4;
 }
@@ -209,8 +235,8 @@ public:
 };
 
 enemigo_sargento::enemigo_sargento(){
-	img[0][0]=0;img[0][1]=178;img[0][2]=207;img[0][3]=178;img[0][4]=0;
-	img[1][0]=0;img[1][1]=0;img[1][2]=193;img[1][3]=0;img[1][4]=0;
+	img[0][0]=204;img[0][1]=205;img[0][2]=207;img[0][3]=205;img[0][4]=185;
+	img[1][0]=0;img[1][1]=0;img[1][2]=202;img[1][3]=0;img[1][4]=0;
 	color=3;
 }
 
@@ -221,8 +247,8 @@ public:
 };
 
 enemigo_teniente::enemigo_teniente(){
-	img[0][0]=196;img[0][1]=178;img[0][2]=207;img[0][3]=178;img[0][4]=196;
-	img[1][0]=0;img[1][1]=193;img[1][2]=0;img[1][3]=193;img[1][4]=0;
+	img[0][0]=201;img[0][1]=205;img[0][2]=207;img[0][3]=205;img[0][4]=187;
+	img[1][0]=202;img[1][1]=193;img[1][2]=0;img[1][3]=193;img[1][4]=202;
 	color=14;
 }
 
@@ -233,13 +259,13 @@ public:
 };
 
 enemigo_general::enemigo_general(){
-	img[0][0]=196;img[0][1]=178;img[0][2]=207;img[0][3]=178;img[0][4]=196;
-	img[1][0]=207;img[1][1]=193;img[1][2]=207;img[1][3]=193;img[1][4]=207;
+	img[0][0]=201;img[0][1]=205;img[0][2]=207;img[0][3]=205;img[0][4]=187;
+	img[1][0]=200;img[1][1]=205;img[1][2]=206;img[1][3]=205;img[1][4]=188;
 	color=2;
 }
 
 // Tiempo Global
-int periodoRefresco = 120; // a menor valor, mas rapidez de refresco
+int periodoRefresco = 70; // a menor valor, mas rapidez de refresco
 int ultimoRefresco = clock(); // guardamos el tiempo actual
 
 //FORMACION
@@ -249,9 +275,8 @@ class oleada: public enemigo_sargento,public enemigo_infanteria, public enemigo_
 	void cargar_enemigo(int tipo,int posicion,int fila);
 public:
 	oleada(int config_f1[2],int config_f2[2], int config_f3[2], int config_f4[2], int config_f5[2], int config_f6[2]);
-	int resto_fila(int enemigos);
 	void dibujar_oleada();
-	
+	bool colision_oleada(int _x, int _y);
 };
 
 oleada::oleada(int config_f1[2],int config_f2[2], int config_f3[2], int config_f4[2], int config_f5[2], int config_f6[2]){
@@ -297,9 +322,9 @@ void oleada::cargar_enemigo(int tipo,int posicion,int fila){
 		formacion.push_back(new enemigo_infanteria());
 	}
 	formacion.back()->setFila(fila);
-	formacion.back()->setY(20 - (2 * fila));
-	formacion.back()->setX(15 + (5* posicion));
-	formacion.back()->setDir(IZQ);
+	formacion.back()->setY(ALTURA_OLEADA - (ALTURA_NAVE * fila));
+	formacion.back()->setX(LIMITE_IZQ_OLEADA + ((ANCHO_NAVE + 1) * posicion));
+	formacion.back()->setDir(DER);
 	formacion.back()->setVida(1);
 	formacion.back()->setPosicion(posicion);
 }
@@ -307,28 +332,46 @@ void oleada::cargar_enemigo(int tipo,int posicion,int fila){
 void oleada::dibujar_oleada(){
 	int t = formacion.size();
 	for(int i = 0; i < t; i++){
-		formacion[i]->borrar();
-		formacion[i]->mover();
+		if(formacion[i]!= NULL){
+			formacion[i]->borrar();
+			formacion[i]->mover();
+		}
 	}
 }
+
+
+bool oleada::colision_oleada(int _x, int _y){
+	int t = formacion.size();
+	for(int i = 0; i < t; i++){
+		if(formacion[i]!= NULL){
+			if(formacion[i]->colision_nave(_x,_y)){
+				formacion[i]->borrar();
+				formacion[i]=NULL;
+				gotoxy(105,10);
+				int p=i;
+				cout<<"PUNTAJE: "<<p;
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
 int main(int argc, char *argv[]) {
 	pintar_limites();
 	nave *n1 = new nave();
 	//Difinición de NIVEL
-	int fila_1[2] = {10,1}; 
+	int fila_1[2] = {2,1}; 
 	int fila_2[2] = {10,1};
 	int fila_3[2] = {10,1};
 	int fila_4[2] = {8,2};
 	int fila_5[2] = {6,3};
 	int fila_6[2] = {4,4};
-	
 	//Difinición de NIVEL
 	oleada *nivel1 = new oleada(fila_1,fila_2,fila_3,fila_4,fila_5,fila_6);
 	n1->setX(38);n1->setY(30);n1->setVida(3);
 	n1->pintar();
 	n1->pintar_vida();
-	
-	//nivel1->posicionar_oleada();
 	
 	while(true){
 		ShowConsoleCursor(false);
@@ -337,14 +380,17 @@ int main(int argc, char *argv[]) {
 			char t = getch();
 			n1->borrar();
 			n1->mover(t);
-			if(t == 'a')
-			   n1->disparar();
-			
+			if(t == 'a'){
+				n1->disparar();
+			}
 		}
-		
+
 		if(clock() > ultimoRefresco + periodoRefresco)
 		{
-			n1->mover_disparo();
+			if(n1->bala_activa()){
+				n1->mover_disparo();
+				if(nivel1->colision_oleada(n1->getX_B(),n1->getY_B()))n1->eliminar_disparo();
+			}
 			nivel1->dibujar_oleada();
 			ultimoRefresco = clock();
 		}
